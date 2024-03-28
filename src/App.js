e6ac26233b7a0036bd068adfd711666d5b916e53
Tharhtet13 <156881594+Tharhtet13@users.noreply.github.com>
2024-03-29 00:51:42 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import './normal.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="side-menu">
+        <div className="side-menu-buttom">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+            <div className="avatar">
+            
+            </div>
+            <div className="message">
+              Hello world
+            </div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+            <div className="avatar chatgpt">
+            
+            </div>
+            <div className="message">
+              I am Library Email Assistant AI
+            </div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea rows="1"
+          className="chat-input-textarea" 
+          placeholder="Type a message..."></textarea>
+        </div>
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
